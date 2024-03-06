25fb2860347dfe0a37eddbce70d1ad0cb06c01db
angelespardoparedes <angeles.pardopared@gmail.com>
2024-03-06 20:58:50 +0100

    index

--- index.js
+++ index.js
@@ -1,68 +1,20 @@
-// librerías importadas
 const express = require("express");
-const cors = require("cors");
-// componentes "míos" que voy a utilizar
-const HTTPSTATUSCODE = require("./utils/httpStatusCode");
-const { connectMongo } = require("./utils/db");
-const trackRouter = require("./src/routes/track.routes");
+const connectMongo = require("./utils/mongoConnection"); // Importamos la función de conexión a la base de datos
+const recipeRoutes = require("./routes/recipeRoutes");
 
-// imports de routes
-
-//
-
-// CONFIGURACION
-const PORT = 3000;
-connectMongo();
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-/* app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:4200'],
-    credentials: true,
-})); */
-app.use(cors());
+// Middleware para manejar solicitudes JSON
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-/* ROUTES */
-//app.use('/track', trackRouter);
-//app.use('/contributor', contributorRouter);
-//app.use('/release', releaseRouter);
-//app.use('/user', userRouter);
+// Conexión a la base de datos MongoDB
+connectMongo();
 
-// ruta de bienvenida
-app.get("/", (request, response) => {
-  response.status(200).json({
-    message: "Welcome to my server",
-    app: "My App",
-  });
-});
+// Rutas de la API
+app.use("/recipes", recipeRoutes);
 
-app.use("track");
-
-/* MANEJO DE ERRORES */
-
-app.use((request, response, next) => {
-  let error = new Error();
-  error.status = 404;
-  error.message = HTTPSTATUSCODE[404];
-  next(error);
-});
-
-app.use((error, request, response, next) => {
-  return response
-    .status(error.status || 500)
-    .json(error.message || "Unexpected error");
-});
-
-app.disable("x-powered-by");
-
-/* DEFINIR EL PUERTO E INICIAR LA ESCUCHA */
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`app running in port ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
